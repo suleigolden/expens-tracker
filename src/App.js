bd46a88b702232fc691881b70e0387272f598811
Suleiman Mamman <suleigolden>
2020-12-31 08:13:52 +0100

    balance component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import {Header} from './component/Header';
+import {Balance} from './component/Balance';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }

d451ed5e0acea9928e3265b3843cd505a9e60e05
Suleiman Mamman <suleigolden>
2021-01-14 15:32:06 +0100

    add transaction action implemented

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -27,9 +27,16 @@ export const GlobalProvider = ({ children }) => {
             payload: id
         });
     }
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
-        deleteTransaction
+        deleteTransaction,
+        addTransaction
     }}>
         {children}
     </GlobalContext.Provider>)

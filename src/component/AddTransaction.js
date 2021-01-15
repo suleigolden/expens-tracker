64a4f74ee1519eb6c0f04a821f36782c895ee147
Suleiman Mamman <suleigolden>
2021-01-15 11:36:39 +0100

    tags rearrange

--- src/component/AddTransaction.js
+++ src/component/AddTransaction.js
@@ -29,10 +29,8 @@ export const AddTransaction = () => {
                 <input type="text" value={text} onChange={ (e) => setText(e.target.value) } placeholder="Enter text..." />
                 </div>
                 <div className="form-control">
-                <label htmlFor="amount"
-                    >Amount <br />
-                    (negative - expense, positive - income)</label
-                >
+                <label htmlFor="amount">Amount <br />
+                    (negative - expense, positive - income)</label>
                 <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
